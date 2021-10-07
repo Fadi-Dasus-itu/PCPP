55aea906188d9f52c6a8db97770ce99261507a5d
Vivian Seerup <vise@itu.dk>
2021-10-08 00:27:23 +0200

    added a folder with vise exercise 6 and 5

---
+++ vise-week6&5/week06/Exercises/LongCounter.java
@@ -0,0 +1,9 @@
+class LongCounter {
+  private long count = 0;
+  public synchronized void increment() {
+    count = count + 1;
+  }
+  public synchronized long get() { 
+    return count; 
+  }
+}
